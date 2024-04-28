945293f6286ae6b3963c6482a48f18526b1fe37f
len-mendonca <lenmendonca2003@gmail.com>
2024-04-28 14:21:41 +0530

    nodemailer: better alternative for resend

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,35 +1,55 @@
-import { Resend } from "resend";
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: "lenmendonca2003@outlook.com",
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
+
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+  async function main() {
+    const info = await transporter.sendMail({
+      from: '"AUTHV5" <lenmendonca2003@outlook.com>',
+      to: email,
+      subject: email,
+      text: "hi?",
+      html: `<p>click <a href="${confirmLink}">here </a>   to verify email</p>`,
+    });
+  }
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "lenmendonca2003@gmail.com",
-    subject: email,
-    html: `<p>click <a href="${confirmLink}">here </a>   to verify email</p>`,
-  });
+  await main().catch(console.error);
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`;
+  async function main() {
+    const info = await transporter.sendMail({
+      from: '"AUTHV5" <lenmendonca2003@outlook.com>',
+      to: email,
+      subject: email,
+      text: "hi?",
+      html: `<p>click <a href="${confirmLink}">here </a> to reset your password</p>`,
+    });
+  }
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "lenmendonca2003@gmail.com",
-    subject: email,
-    html: `<p>click <a href="${confirmLink}">here </a> to reset your password</p>`,
-  });
+  await main().catch(console.error);
 };
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "lenmendonca2003@gmail.com",
-    subject: email,
-    html: `<p>Your two-factor code is ${token}</p>`,
-  });
+  async function main() {
+    const info = await transporter.sendMail({
+      from: '"AUTHV5" <lenmendonca2003@outlook.com>',
+      to: email,
+      subject: email,
+      text: "hi?",
+      html: `<p>Your two-factor code is ${token}</p>`,
+    });
+  }
+
+  await main().catch(console.error);
 };
